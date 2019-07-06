429d23c89306465d6505582e3847b00de40fac2f
Dustin Knopoff <dustinknopoff@gmail.com>
2019-07-06 08:50:57 -0400

    feat(cms): easier for adding content

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,12 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
 }
